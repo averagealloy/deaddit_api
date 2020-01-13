81504536fd1c14a1c496fc97948cd4d7d00a21bb
averagealloy <m.mctighe10@gmail.com>
2020-01-13 13:17:46 -0500

    added index and show to controller

--- app/controllers/api/num1/posts_controller.rb
+++ app/controllers/api/num1/posts_controller.rb
@@ -1,7 +1,14 @@
-class Api::num1::DeaditController < ApplicationController
+class Api::num1::PostsController < ApplicationController
     def index 
         @posts = Post.All
 
         render json: @posts, status: 200
     end 
+
+    def show
+        @post = Post.find(params[:id])
+
+        render json: @post, status: 200
+    end
+    
 end
