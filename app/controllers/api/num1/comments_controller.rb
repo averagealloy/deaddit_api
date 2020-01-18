34cb7bc29bc2461cf0bccc8d02ff027c0740318a
averagealloy <m.mctighe10@gmail.com>
2020-01-18 18:14:11 -0500

    added some structure to comments controller

---
+++ app/controllers/api/num1/comments_controller.rb
@@ -0,0 +1,18 @@
+class Api::Num1::CommentsController < ApplicationController
+    def index
+        @post = Post.find(params[:id])
+        comments = 
+
+    def show
+    end 
+
+    def create
+    end 
+
+    def update
+    end 
+
+    def destroy
+
+
+end 
